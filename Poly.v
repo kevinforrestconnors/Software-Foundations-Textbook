cefbcba524da84e03c86d0cdf88851788e2e8af9
Kevin Connors <kevinforrestconnors@gmail.com>
2016-02-21 23:37:37 -0600

    add snoc_with_append

--- Poly.v
+++ Poly.v
@@ -432,8 +432,17 @@ Theorem snoc_with_append : forall X : Type,
                          forall v : X,
   snoc (l1 ++ l2) v = l1 ++ (snoc l2 v).
 Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
+  intros X l1 l2 v.
+  induction l1.
+  Case "[] ++ l2".
+    simpl.
+    reflexivity.
+  Case "l1 ++ l2".
+    simpl.
+    rewrite -> IHl1.
+    reflexivity.
+Qed.
+
 
 (* ###################################################### *)
 (** ** Polymorphic Pairs *)
